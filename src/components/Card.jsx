fb59882eece6e5b573c6dd4fe0f544057f45be42
Melodi <63905351+melodidev@users.noreply.github.com>
2023-08-10 18:24:56 +0300

    Update card image URL

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card(props) {
       onClick={props.handleClick}
       className="cursor-pointer d-flex flex-column align-items-center border border-2 bg-light bg-opacity-25 rounded mb-lg-4 mb-3"
     >
-      <img src={`public/images/${props.img}`} className="img-fluid mt-3 mb-2" />
+      <img src={`${import.meta.env.BASE_URL}images/${props.img}`} className="img-fluid mt-3 mb-2" />
       <div className="text-light">{props.name}</div>
     </div>
   )
